43d39522f6f94a8533b3f3ecd01f6280bfa6a9ed
Dmitry Moskvenkov <52528916+moskvenkov@users.noreply.github.com>
2020-12-27 11:17:50 +0300

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -8,9 +8,9 @@ rsync -avzp --stats --progress /mnt/freenas/main/ /data/main/ >> /data/logs/$tim
 sync
 echo 'rsync done'
 xdelta3 -f -s /data/1c/acc/buh.1CD /data/main/1c/1CBases/BUH/acc2019/1Cv8.1CD /data/1c/acc/$timestamp.xdelta
-xdelta3 -f -s /data/1c/zup/%ORGNAME%/zup.vvv.1CD /data/main/1c/1CBases/ЗУП/3.0/ВВВ/1Cv8.1CD /data/1c/zup/%ORGNAME%/$timestamp.xdelta
-xdelta3 -f -s /data/1c/zup/%ORGNAME%/zup.igit.1CD /data/main/1c/1CBases/ЗУП/3.0/ИГИТ/1Cv8.1CD /data/1c/zup/%ORGNAME%/$timestamp.xdelta
-xdelta3 -f -s /data/1c/zup/%ORGNAME%/zup.ysa.1CD /data/main/1c/1CBases/ЗУП/3.0/ЯСА/1Cv8.1CD /data/1c/zup/%ORGNAME%/$timestamp.xdelta
+xdelta3 -f -s /data/1c/zup/%ORGNAME%/zup.vvv.1CD /data/main/1c/1CBases/ЗУП/3.0/%ORGNAME%/1Cv8.1CD /data/1c/zup/%ORGNAME%/$timestamp.xdelta
+xdelta3 -f -s /data/1c/zup/%ORGNAME%/zup.igit.1CD /data/main/1c/1CBases/ЗУП/3.0/%ORGNAME%/1Cv8.1CD /data/1c/zup/%ORGNAME%/$timestamp.xdelta
+xdelta3 -f -s /data/1c/zup/%ORGNAME%/zup.ysa.1CD /data/main/1c/1CBases/ЗУП/3.0/%ORGNAME%/1Cv8.1CD /data/1c/zup/%ORGNAME%/$timestamp.xdelta
 log_end_date=`date +%Y-%m-%d-%H-%H-%S`
 echo $log_end_date >> /data/logs/$timestamp.log
 sync
